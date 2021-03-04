5aaf4967b43a4a1550dfed800c91c7c259c8c10d
Weslley Lima <weslley.lima@zup.com.br>
2021-03-04 13:28:41 -0300

    Iniciar o projeto Spring

---
+++ src/test/java/br/com/zup/Produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
